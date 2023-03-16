b92776a8f159143e23a94d38204a7f09f83cb46c
Sergittko <derov180214@gmail.com>
2023-03-16 15:29:47 +0200

    filter adaptive layout

--- src/components/MainContent/SneakersSection/FilterSneakers/FilterSneakers.jsx
+++ src/components/MainContent/SneakersSection/FilterSneakers/FilterSneakers.jsx
@@ -7,30 +7,50 @@ import PriceFilter from "./Filters/PriceFilter/PriceFilter";
 import YearFilter from "./Filters/YearFilter/YearFilter";
 import { ReactComponent as ChevroneUp } from "../../../../img/icons/circleChevronUp.svg";
 import { Link as ScrollLink } from "react-scroll";
+import { useState } from "react";
 
 let FilterSneakers = ({ setFilterData }) => {
   const { t } = useTranslation();
+  let [showAllFilters, setShowAllFilters] = useState(false);
+
   return (
     <aside className={style.filterWrapper}>
       <div>
-        <h3>{t("sneakersSection.filterSneakers.filterTitle")}</h3>
-        <ModelFilter setFilterData={setFilterData} />
-        <BrandFilter setFilterData={setFilterData} />
-        <SizeFilter setFilterData={setFilterData} />
-        <PriceFilter setFilterData={setFilterData} />
-        <YearFilter setFilterData={setFilterData} />
-      </div>
-
-      <div className={style.buttonUp}>
-        <ScrollLink
-          to="sneakerResultContainer"
-          smooth={true}
-          offset={-100}
-          duration={500}
+        <h3
+          onClick={() =>
+            window.innerWidth < 760 && setShowAllFilters(!showAllFilters)
+          }
         >
-          <ChevroneUp className={style.chevroneUp} />
-        </ScrollLink>
+          {t("sneakersSection.filterSneakers.filterTitle")}
+        </h3>
+        <div
+          className={
+            window.innerWidth < 760
+              ? showAllFilters === true
+                ? style.showAllFilters
+                : style.hideAllFilters
+              : ""
+          }
+        >
+          <ModelFilter setFilterData={setFilterData} />
+          <BrandFilter setFilterData={setFilterData} />
+          <SizeFilter setFilterData={setFilterData} />
+          <PriceFilter setFilterData={setFilterData} />
+          <YearFilter setFilterData={setFilterData} />
+        </div>
       </div>
+      {window.innerWidth > 760 && (
+        <div className={style.buttonUp}>
+          <ScrollLink
+            to="sneakerResultContainer"
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            <ChevroneUp className={style.chevroneUp} />
+          </ScrollLink>
+        </div>
+      )}
     </aside>
   );
 };
